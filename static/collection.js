62e0d75e55c3a532f9df92e327972fc84b172990
Euan <euanconlon46@gmail.com>
2026-07-14 20:38:21 +0100

    Constants Created

---
+++ static/collection.js
@@ -0,0 +1,6 @@
+const minifigures = JSON.parse(document.getElementsById('minifigure-data').textContent);
+const grid = document.getElementById('collection-grid');
+const search = document.getElementById('search');
+const filterTheme = document.getElementById('filter-theme');
+const filterYear = document.getElementById('filter-year');
+const filterPrice = document.getElementById('filter-price');
